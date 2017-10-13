c077c55d2b26ee92dd9b191f23f0cb738c3b1702
David Brennan <ddbren@gmail.com>
2017-10-13 22:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < musicians.length; i++) {
     band.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+  console.log(band[1])
   return band
 }
+
+
