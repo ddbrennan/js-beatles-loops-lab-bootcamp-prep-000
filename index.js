309ed24ec23c6ae37a28850c83c757e001d62af3
David Brennan <ddbren@gmail.com>
2017-10-13 22:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < musicians.length; i++) {
     band.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  console.log(band[1])
   return band
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return facts
 }
